84a76dcd96f75da9f3f591697d8ae986887290e4
Hailey Pate <2haileypate@gmail.com>
2018-11-18 15:56:00 -0600

    shame commit

--- themes/material-components/assets/js/main.js
+++ themes/material-components/assets/js/main.js
@@ -1,6 +1,8 @@
 import {MDCRipple} from '../../node-modules/@material/ripple';
 import {MDCTopAppBar} from '../../node-modules/@material/top-app-bar/index';
 import {MDCList} from '../../node-modules/@material/list/index';
+import {MDCTab} from '../../node-modules/@material/tab/index';
+import {MDCDrawer} from '../../node-modules/@material/drawer/index';
 
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
@@ -10,3 +12,10 @@ const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
 
 const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
 iconButtonRipple.unbounded = true;
+
+const tab = new MDCTab(document.querySelector('.mdc-tab'));
+
+const list = MDCList.attachTo(document.querySelector('.mdc-list'));
+list.wrapFocus = true;
+
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
